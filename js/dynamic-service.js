103082e362c7286266343b90ec9e085ebfe06423
DravenPorschE <dravenporsch@gmai.com>
2026-03-02 16:53:13 +0800

    alsdasldkkjaldksjlkk

--- js/dynamic-service.js
+++ js/dynamic-service.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     const typeInfo = document.getElementById("type-info");
     const backBtn = document.querySelector(".back-button");
 
-    const dropdownAction = document.querySelectorAll(".inner-process-info");
+    const dropdownAction = document.querySelectorAll(".client-step-container");
 
     const urlParams = new URLSearchParams(window.location.search);
     const val = urlParams.get("search");
@@ -78,7 +78,95 @@ document.addEventListener("DOMContentLoaded", async () => {
             
 
             // Create new steps
+            const steps = matchedService.steps || [];
+            const processInfoContainer = document.querySelector(".process-info");
+ 
+            let agencyActionLabel = document.createElement("p");
+            agencyActionLabel.className = "service-label";
+            agencyActionLabel.textContent = "AgencyActions";           
+            
+            let currentStep = 0;
+            let agencyStepNum = 0;
 
+            steps.forEach((step, index) => {
+
+                if(step.client_step != "N/A") {
+                    agencyStepNum = 0;
+                    currentStep++;
+                    const processContainer = document.createElement("div");
+                    processContainer.className = "process-container";
+
+                    const clientStepContainer = document.createElement("div");
+                    clientStepContainer.className = "client-step-container";
+
+                    const checkboxContainer = document.createElement("div");
+                    checkboxContainer.className = "checkbox-container";
+
+                    const checkbox = document.createElement("div");
+                    checkbox.className = "checkbox";
+
+                    const stepContainer = document.createElement("div");
+                    stepContainer.className = "step-container";
+
+                    let stepNumber = document.createElement("p");
+                    stepNumber.className = "step-number";
+                    if(step.client_step != "N/A") {
+                        stepNumber.textContent = "Step " + currentStep;
+                    } 
+                    
+                    let clientStepInfo = document.createElement("p");
+                    clientStepInfo.className = "client-step-info";
+                    if(step.client_step != "N/A") {
+                        clientStepInfo.textContent = step.client_step
+                            .replace(/^\d+\.\s*/, "")
+                            .trim();
+                    } 
+                    
+                    const svgNS = "http://www.w3.org/2000/svg";
+
+                    // Create the <svg>
+                    const svg = document.createElementNS(svgNS, "svg");
+                    svg.classList.add("step-arrow");
+                    svg.setAttribute("viewBox", "0 0 24 24");
+                    svg.setAttribute("fill", "none");
+                    svg.setAttribute("stroke", "currentColor");
+                    svg.setAttribute("stroke-width", "2");
+                    svg.setAttribute("stroke-linecap", "round");
+                    svg.setAttribute("stroke-linejoin", "round");
+
+                    // Create the <polyline>
+                    const polyline = document.createElementNS(svgNS, "polyline");
+                    polyline.setAttribute("points", "9 18 15 12 9 6");
+
+                    // Append polyline into svg
+                    svg.appendChild(polyline);
+
+                    checkboxContainer.appendChild(checkbox);
+                    stepContainer.appendChild(stepNumber);
+                    stepContainer.appendChild(clientStepInfo);
+                    
+                    clientStepContainer.appendChild(checkboxContainer);
+                    clientStepContainer.appendChild(stepContainer);
+                    clientStepContainer.appendChild(svg);
+
+                    processContainer.appendChild(clientStepContainer);
+
+                    processInfoContainer.appendChild(processContainer);
+                } else {
+                    agencyStepNum++;
+
+                    const agencyActionContainer = document.createElement("div");
+                    agencyActionContainer.className = "agency-action-container";
+
+                    const agencyAction = document.createElement("div");
+                    agencyAction.className = "agency-action";
+
+                    let agencyStep = document.createElement("p");
+                    agencyStep.className = "agency-step";
+                    agencyStep.textContent = currentStep + "." + agencyStepNum;
+
+                }
+            });
         } else {
             console.log("Service not found");
         }
@@ -87,14 +175,21 @@ document.addEventListener("DOMContentLoaded", async () => {
         console.log("No search parameter in URL");
     }
 
-    // Dropdown toggle
-    dropdownAction.forEach(btn => {
-        btn.addEventListener("click", () => {
-            const processContainer = btn.closest(".process-container");
-            const innerProcess = processContainer.querySelector(".inner-process");
+    document.querySelectorAll(".client-step-container").forEach(step => {
+        step.addEventListener("click", () => {
 
-            innerProcess.classList.toggle("show");
+            const processContainer = step.closest(".process-container");
+            const agencyContainer = processContainer.querySelector(".agency-action-container");
+            const checkbox = step.querySelector(".checkbox");
+
+            // Toggle dropdown
+            agencyContainer.classList.toggle("show");
+
+            // Toggle arrow rotation
+            step.classList.toggle("active");
+
+            // Toggle checkbox
+            checkbox.classList.toggle("checked");
         });
     });
-
 });
